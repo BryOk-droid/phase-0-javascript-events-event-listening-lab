2c82ae52c9fb3b16a27ce6b3d720bada3ecd63e4
BryOk-droid <brianokoth534@gmail.com>
2025-03-21 12:37:59 +0300

    Implemented addingEventListener() to attach a click event that triggers an alert

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ function addingEventListener() {
   // Select the button element by ID
   const input = document.getElementById("button");
 
-  // Add a click event listener to the button
   input.addEventListener("click", function () {
     alert("I was clicked!");
   });
 }
 
-// Call the function to attach the event listener
 addingEventListener();
